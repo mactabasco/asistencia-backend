8cd8101197ced84237c95145dfcd3df5b4a076a8
mactabasco <cursosmactabasco@gmail.com>
2026-06-04 12:47:01 -0600

    fix: configurar puerto dinamico y escuchar en 0.0.0.0 para Render

--- src/app.js
+++ src/app.js
@@ -8,7 +8,12 @@ import apiRoutes from './routes/api.js';
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`);
+});
 
 // Middlewares globales
 app.use(cors());
