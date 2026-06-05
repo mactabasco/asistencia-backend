48b398a9dafc5f2db443e248264a03ba8e57b9f0
mactabasco <cursosmactabasco@gmail.com>
2026-06-05 15:02:01 -0600

    Update app.js
    Configuración del puerto 8080

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`);
